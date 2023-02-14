b29ae6aae5a542b9cc5783abbc75cc32256f2107
reecevela <reecevela@outlook.com>
2023-02-14 11:45:11 -0600

    Change file structure

---
+++ scripts/reverse.js
@@ -0,0 +1,4 @@
+export default function reverse(input) {
+    const output = input.split("").reverse().join('');
+    return output;
+}
---
+++ tests/capitalize.test.js
@@ -0,0 +1,17 @@
+import capitalize from "../scripts/capitalize";
+
+test('Capitalizes a single letter', () => {
+    expect(capitalize('a')).toBe('A');
+});
+
+test('Doesn\'t modify numbers', () => {
+    expect(capitalize('123123')).toBe('123123');
+});
+
+test('Only capitalizes first letter', () => {
+    expect(capitalize('hello there')).toBe('Hello there');
+});
+
+test('Works with punctuation', () => {
+    expect(capitalize('hi - how are you?')).toBe('Hi - how are you?');
+});
